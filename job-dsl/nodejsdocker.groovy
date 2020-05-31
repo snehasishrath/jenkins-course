7d84bef759577b96eba79c301cc172fc086f15d0
snsrath <snehasish.rath@gmail.com>
2020-05-31 15:48:05 +0200

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('snsrath/nodejsapp:latest')
+            repositoryName('snsrath/nodejsapp')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
